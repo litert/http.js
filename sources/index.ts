9aea2f6816ec3f28ec759858efe8084cd5bd2970
Angus.Fenying <i.am.x.fenying@gmail.com>
2017-12-04 12:30:51 +0800

    Added copyright in each source file.

--- sources/index.ts
+++ sources/index.ts
@@ -1,3 +1,18 @@
+/*
+   +----------------------------------------------------------------------+
+   | LiteRT HTTP.js Library                                               |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2007-2017 Fenying Studio                               |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.0 of the Apache license,    |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | https://github.com/litert/http.js/blob/master/LICENSE                |
+   +----------------------------------------------------------------------+
+   | Authors: Angus Fenying <i.am.x.fenying@gmail.com>                    |
+   +----------------------------------------------------------------------+
+ */
+
 export {
 
     RequestContext,
